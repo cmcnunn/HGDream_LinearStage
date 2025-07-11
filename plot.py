4000d7ec793bd6bfa9195e17c2425f83eba7e1ac
Colin Nunn <colnunn@ttu.edu>
2025-07-11 15:43:01 -0500

    Added reset function
    This does not currently work because it cannot import reset_plot

--- plot.py
+++ plot.py
@@ -2,36 +2,39 @@ import matplotlib.pyplot as plt
 
 # Initial setup
 fig, ax = plt.subplots()
-dot, = ax.plot([0], [0], 'ro')  # red dot for current position
+dot, = ax.plot([0], [0], 'ro')
 ax.set_xlim(-100, 100)
 ax.set_ylim(-100, 100)
 ax.set_title("Dot Position")
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 
-# Store previous position
 prev_x, prev_y = 0, 0
-
-plt.ion()  # Interactive mode on
+plt.ion()
 plt.show()
 
 def update_dot(x, y):
     global prev_x, prev_y
-
-    # Plot old position as black dot
-    ax.plot(prev_x, prev_y, 'ko')  # small black dot at old position
-
-    # Draw vector (arrow) from old to new
-    dx = x - prev_x
-    dy = y - prev_y
-    ax.arrow(prev_x, prev_y, dx, dy, head_width=3, head_length=6, fc='blue', ec='blue', length_includes_head=True)
-
-    # Update current dot
+    ax.plot(prev_x, prev_y, 'ko')  # leave a black trail
+    ax.arrow(prev_x, prev_y, x - prev_x, y - prev_y,head_width=3, head_length=6, fc='blue', ec='blue', length_includes_head=True)
     dot.set_data([x], [y])
-
-    # Redraw everything
     plt.draw()
     plt.pause(0.01)
-
-    # Update stored position
     prev_x, prev_y = x, y
+
+def reset_plot():
+    global prev_x, prev_y, dot
+    prev_x, prev_y = 0, 0
+
+    ax.cla()  # clear all objects
+
+    # Rebuild axes and dot
+    ax.set_xlim(-100, 100)
+    ax.set_ylim(-100, 100)
+    ax.set_title("Dot Position")
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    dot, = ax.plot([0], [0], 'ro')  # recreate red dot at center
+
+    plt.draw()
+    plt.pause(0.01)
